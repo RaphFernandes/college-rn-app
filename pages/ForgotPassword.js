845570d3f88e8a15fc0b8ece0674d3f3689e48e5
Raphael <fernandeeraphael@gmail.com>
2024-04-05 11:15:01 -0300

    Adding challenge 1 features

---
+++ pages/ForgotPassword.js
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+import { styles } from "../styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text>RECUPERE SUA SENHA</Text>
+      </View>
+    </View>
+  );
+}
---
+++ pages/Login.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { Link } from "@react-navigation/native";
+import Input from "../components/Input";
+import { styles } from "../styles";
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEmailChange = (value) => {
+    if (errorMessage?.toLowerCase()?.includes("e-mail")) {
+      setErrorMessage("");
+    }
+
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    if (errorMessage?.toLowerCase()?.includes("senha")) {
+      setErrorMessage("");
+    }
+
+    setPassword(value);
+  };
+
+  const handleLoginButton = () => {
+    
+    if (!email) {
+        setErrorMessage("E-mail não pode estar vazio");
+        return;
+      }
+  
+      if (!password) {
+        setErrorMessage("Senha não pode estar vazia");
+        return;
+      }
+  
+    Alert.alert("Login bem sucedido");
+    setEmail("");
+    setPassword("");
+
+    navigation.navigate("Home");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+      {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+      </View>
+
+      <Input
+        label="E-mail"
+        placeholder="lorem@ipsum"
+        value={email}
+        onChange={handleEmailChange}
+      />
+
+      <Input
+        label="Senha"
+        placeholder="********"
+        value={password}
+        onChange={handlePasswordChange}
+        secureTextEntry
+      />
+
+      <View style={styles.wrapper}>
+        <Link style={styles.link} to="/ForgotPassword">
+          Esqueci minha senha.
+        </Link>
+      </View>
+
+      <View style={styles.wrapper}>
+        <Link style={styles.link} to="/Register">
+          Cadastre-se
+        </Link>
+      </View>
+
+      <View style={styles.wrapper}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLoginButton}
+          accessibilityLabel="Botão para entrar com sua conta"
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
